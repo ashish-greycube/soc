0421bc43972cb609ed2ea3a401b6e1d3ad7f1236
ashish-greycube <mr.ashish.shah@gmail.com>
2020-06-21 06:44:55 +0200

    fix: revert trackme

--- soc/hooks.py
+++ soc/hooks.py
@@ -16,8 +16,6 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/soc/css/soc.css"
-app_include_js = ["/assets/soc/js/lib/ag-grid-community.min.js"]
-
 # include js, css files in header of web template
 # web_include_css = "/assets/soc/css/soc.css"
 # web_include_js = "/assets/soc/js/soc.js"
